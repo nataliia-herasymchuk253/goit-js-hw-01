0725e610748802793e024058a64fdbbecc73c570
Nataliia <od170383bno@gmail.com>
2024-12-01 15:10:26 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,10 @@
 
 function makeTransaction(quantity, pricePerDroid) {
-  
-  const totalPrice = quantity * pricePerDroid;
-  
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    
+    const totalPrice = quantity * pricePerDroid;
+
+    
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
